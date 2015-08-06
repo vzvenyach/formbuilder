a0f3709f2576d192d0423d6db9d2432497309bb6
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-08-06 16:43:48 -0500

    New features
    * New select form element
    * Slight improvement for null labels
    * Beginning of a setAttr function

--- tests/testBuild.js
+++ tests/testBuild.js
@@ -4,7 +4,7 @@ var yml = require('js-yaml');
 var BuildForm = require('../lib/buildform.js');
 
 test('build test', function (t) {
-	t.plan(3)
+	t.plan(4)
   	var doc = yml.safeLoad(fs.readFileSync(__dirname + '/test.yml', 'utf8'));  
     
     var form = new BuildForm(); // Initialize the BuildForm module
@@ -14,6 +14,10 @@ test('build test', function (t) {
     form = new BuildForm(doc);	// Initialize the BuildForm module
     t.equal(form.d.controls, doc.controls)
 
-    console.log("testing for BuildForm form")    
-    t.equal(form.makeForm().join(''),'<div class="form-group"><label for="question" class="control-label">Ask a question</label>\n<textarea class="form-control" rows="3" id="question" name="question" placeholder="Ask a question"></textarea></div><div class="form-group"><label for="tags" class="control-label">undefined</label>\n<input class="form-control" id="tags" name="tags" type="text" placeholder="Add a tag"></div>')
+    console.log("testing for BuildForm form")
+    t.equal(form.makeForm().join(''),'<div class="form-group"><label for="question" class="control-label">Ask a question</label>\n<textarea class="form-control" rows="3" id="question" name="question" placeholder="Ask a question"></textarea></div><div class="form-group">\n<input class="form-control" id="tags" name="tags" type="text" placeholder="Add a tag"></div><div class="form-group"><select class="form-control"><option>Option 1</option><option>Option 2</option></select>')
+
+    console.log("testing the setAttr")
+    t.equal(form.setAttr('<div class="form-group">', "test","value"), '<div test="value" class="form-group">')
+
 });
